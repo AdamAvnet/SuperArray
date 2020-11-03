c248ed8a20c28ffa019d4bc09fb41605c7690e63
AdamAvnet <aavnet20@stuy.edu>
2020-11-03 13:25:58 -0500

    Code with methods through toArray() compiles

--- SuperArray.java
+++ SuperArray.java
@@ -8,6 +8,11 @@ public class SuperArray {
 		size = 0;
 	}
 
+	public SuperArray(int InitialCapacity){
+		data = new String[InitialCapacity];
+		size = 0;
+	}
+
 	public int size(){
 		return size;
 	}
@@ -36,6 +41,77 @@ public class SuperArray {
 			newdata[i] = data[i];
 		}
 		data = newdata;
+	}
+
+	public boolean isEmpty(){
+		if(size == 0)
+			return true;
+		else
+			return false;
+	}
+
+	public String toString(){
+		String output = "[";
+		for(int i = 0; i < size; i++){
+			output += data[i];
+			if( i < size - 1)
+				output += ", ";
+		}
+		output += "]";
+		return output;
+	}
+
+	public boolean contains(String s){
+		for(int i = 0; i < size; i++){
+			if(data[i].equals(s))
+				return true;
+		}
+		return false;
+	}
+
+	public void clear(){
+		data = new String[data.length];
+		size = 0;
+	}
+
+	public void add(int index, String element){
+		if(size == data.length)
+			resize();
+		for(int i = size; i >= 0; i--){
+			if(i > index)
+				data[i + 1] = data[i];
+			if(i == index)
+				data[i] = element;
+		}
+		size += 1;
+	}
+
+	public String remove(int index){
+		String value = "";
+		for(int i = 0; i < size; i++){
+			if(i == index)
+				value = data[i];
+			if(i >= index)
+				data[i] = data[i + 1];
+		}
+		size -= 1;
+		return value;
+	}
+
+	public int indexOf(String s){
+		for(int i = 0; i < size; i++){
+			if(data[i].equals(s))
+				return i;
+		}
+		return -1;
+	}
+
+	public String[] toArray(){
+		String[] arr = new String[data.length];
+		for(int i = 0; i < size; i++){
+			arr[i] = data[i];
+		}
+		return arr;
 	}
 }
 
