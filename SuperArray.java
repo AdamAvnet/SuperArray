497fc597f0421c2cbd843d07f798336af32c9ee5
AdamAvnet <aavnet20@stuy.edu>
2020-11-06 16:08:16 -0500

    Fixed a small problem

--- SuperArray.java
+++ SuperArray.java
@@ -105,7 +105,7 @@ public class SuperArray {
 	}
 
 	public String[] toArray(){
-		String[] arr = new String[data.length];
+		String[] arr = new String[size];
 		for(int i = 0; i < size; i++){
 			arr[i] = data[i];
 		}
